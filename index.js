bd568bbf72d6ac4920547418c65259dbf9008594
agufedee <agufedee@gmail.com>
2021-10-14 20:37:44 -0300

    js agregado a los botones

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var abrirCookies = document.getElementById("popupCookies"),
     abrirSesion = document.getElementById("popupSesion"),
     overlay = document.getElementById("overlay"),
+    cookies = document.getElementById("popup1"),
     overlay2 = document.getElementById("overlay2"),
     cerrarCookies = document.getElementById("cerrarPopupCookies"),
     cerrarSesion = document.getElementById("cerrarPopupSesion");
