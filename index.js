2ccab1a2eb48c7dbee7c7f5db97faba39fd49835
agufedee <agufedee@gmail.com>
2021-10-14 19:27:33 -0300

    js agregado a botones

---
+++ index.js
@@ -0,0 +1,22 @@
+var abrirCookies = document.getElementById("popupCookies"),
+    abrirSesion = document.getElementById("popupSesion"),
+    overlay = document.getElementById("overlay"),
+    overlay2 = document.getElementById("overlay2"),
+    cerrarCookies = document.getElementById("cerrarPopupCookies"),
+    cerrarSesion = document.getElementById("cerrarPopupSesion");
+
+abrirCookies.addEventListener("click", () => {
+    overlay.classList.add("active");
+});
+
+cerrarCookies.addEventListener("click", () => {
+    overlay.classList.remove("active");
+});
+
+abrirSesion.addEventListener("click", () => {
+    overlay2.classList.add("active");
+});
+
+cerrarSesion.addEventListener("click", () => {
+    overlay2.classList.remove("active");
+});
